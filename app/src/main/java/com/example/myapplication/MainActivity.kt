cf51f604134f7cc27426034f5e174d6efc79190c
yoonjiyeon <tnstn4130!>
2020-10-05 19:29:21 +0900

    두번째스터디

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setTimeButton()
+    }
+
+    private fun setTimeButton(){
+        time_button.setOnClickListener {
+            showMessage("Time")
+        }
+    }
+
+    private fun getCurrentTime():String{
+        return Calendar.getInstance().time.toString()
+    }
+
+    private fun showMessage(message:String){
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
 }
